184f9ad5e19cbf469c4a53c0f631e3e59488b6ac
maiquelknechtel <maiquelknechtel@maiquelknechtels-MacBook-Pro.local>
2020-11-07 19:33:20 -0300

    Ao entregar aparelho nao permite edição

--- src/app/aparelho-detail/aparelho-detail.component.ts
+++ src/app/aparelho-detail/aparelho-detail.component.ts
@@ -2,78 +2,85 @@ import { Component, NgModule, OnInit } from '@angular/core';
 import { Aparelho } from '../models/aparelhos-component';
 import { ActivatedRoute } from '@angular/router';
 import { AparelhoService } from '../service/aparelho.service';
-import { FormGroup, FormControl,NgForm  } from '@angular/forms';
+import { FormGroup, FormControl, NgForm } from '@angular/forms';
 @Component({
   selector: 'app-aparelho-detail',
   templateUrl: './aparelho-detail.component.html',
   styleUrls: ['./aparelho-detail.component.css']
 })
 export class AparelhoDetailComponent implements OnInit {
-  form1: FormGroup;
-  
-  aparelho ={} as Aparelho;
-  prontoCheck:boolean
-  entregueCheck:boolean
+  form: FormGroup;
+  aparelho = {} as Aparelho;
+  prontoCheck: boolean;
+  entregueCheck: boolean;
+  aparelhoEntregue:boolean =false;
 
-  
-  constructor(private route: ActivatedRoute,private aparelhoService:AparelhoService) { }
+  constructor(private route: ActivatedRoute, private aparelhoService: AparelhoService) { }
 
   async ngOnInit() {
-    
-    const id =Number(this.route.snapshot.paramMap.get('id'))
+
+    const id = Number(this.route.snapshot.paramMap.get('id'))
     this.aparelho.id = id;
     this.aparelho = await this.aparelhoService.getAparelho(this.aparelho).toPromise()
-    console.log("olha aqui -> "+this.aparelho.entregue)
-    if(this.aparelho.pronto==='PRONTO'){
-      this.aparelho.pronto='PRONTO'
-      this.prontoCheck=true;
-    }else{
-      this.aparelho.pronto='NAO_PRONTO'
-      this.prontoCheck=false;
+
+    if (this.aparelho.pronto === this.PRONTO) {
+      this.aparelho.pronto = this.PRONTO
+      this.prontoCheck = true;
+    } else {
+      this.aparelho.pronto = this.NAO_PRONTO
+      this.prontoCheck = false;
     }
-    if(this.aparelho.entregue==='ENTREGUE'){
-      this.aparelho.entregue = 'ENTREGUE'
-      this.entregueCheck=true;
-    }else{
-      this.aparelho.entregue ='NAO_ENTREGUE'
-      this.entregueCheck=false;
+    if (this.aparelho.entregue === this.ENTRGUE) {
+      this.aparelho.entregue = this.ENTRGUE
+      this.entregueCheck = true;
+    } else {
+      this.aparelho.entregue = this.NAO_ENTRGUE
+      this.entregueCheck = false;
     }
-    
-    this.form1 = new FormGroup({
-      
-      nome: new FormControl(''),      
+    if (this.aparelho.entregue === this.ENTRGUE) {
+      this.aparelhoEntregue = true
+    }
+
+    this.form = new FormGroup({
+
+      nome: new FormControl(''),
       modelo: new FormControl(''),
       serial: new FormControl(''),
       defeito_obs: new FormControl(''),
       email: new FormControl(''),
-      pronto:new FormControl(this.prontoCheck),
+      pronto: new FormControl(this.prontoCheck),
       valor: new FormControl(''),
-      entregue: new FormControl(this.entregueCheck) 
+      entregue: new FormControl(this.entregueCheck)
     })
   }
 
-  async doEdit(form1:NgModule){
-    
+  async doEdit(form1: NgModule) {
+
     this.aparelho = await this.aparelhoService.postAparelho(this.aparelho).toPromise()
-    alert("Aparelho editado com sucesso! - >"+this.aparelho.pronto+" valor de aparelho entregue "+this.aparelho.entregue)
+    alert("Aparelho editado com sucesso! - >" + this.aparelho.pronto + " valor de aparelho entregue " + this.aparelho.entregue)
   }
-  eventCheckEntregue(event){
-    if(event.checked){
-      this.aparelho.entregue = 'ENTREGUE'
-      this.entregueCheck=true;
-    }else{
-      this.aparelho.entregue = 'NAO_ENTRGUE'
-      this.entregueCheck=false;
+  eventCheckEntregue(event) {
+    if (event.checked) {
+      this.aparelho.entregue = this.ENTRGUE
+      this.entregueCheck = true;
+    } else {
+      this.aparelho.entregue = this.NAO_ENTRGUE
+      this.entregueCheck = false;
     }
   }
-  eventCheckPronto(event){
-    if(event.checked){
-      this.aparelho.pronto = 'PRONTO'
-      this.prontoCheck=true;
-    }else{
-      this.aparelho.pronto = 'NAO_PRONTO'
-      this.prontoCheck=false;
+  eventCheckPronto(event) {
+    if (event.checked) {
+      this.aparelho.pronto = this.PRONTO
+      this.prontoCheck = true;
+    } else {
+      this.aparelho.pronto = this.NAO_PRONTO
+      this.prontoCheck = false;
     }
   }
 
+
+  private NAO_ENTRGUE = 'NAO_ENTRGUE'
+  private ENTRGUE = 'ENTREGUE'
+  private PRONTO = 'PRONTO'
+  private NAO_PRONTO = 'NAO_PRONTO'
 }
